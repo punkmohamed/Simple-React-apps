b7a378707607d3ca1b76f04ef4c52b57f9ef7c66
punkmohamed <punkmohamed22@gmail.com>
2024-09-03 23:27:17 +0200

    adjusting z-index

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import { gsap } from 'gsap';
 import LoopingTextAnimation from "./animation/TextAnimation";
 const Home = () => {
-    const words = ['Welcome', 'To', 'My', 'Website', 'Enjoy', 'Your', 'These', 'Simple', 'React', 'Apps'];
+    const words = ['Welcome', 'To', 'My', 'Website', 'Enjoy', 'These', 'Simple', 'React', 'Apps'];
     return (
         <div className="d-flex  justify-content-center align-items-center">
             <LoopingTextAnimation words={words} />
